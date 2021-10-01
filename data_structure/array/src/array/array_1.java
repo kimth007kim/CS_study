063de293dc9dd258e47095229b1ee77e9cc78a51
kimth007kim <kimth007kim@naver.com>
2021-10-02 01:58:34 +0900

    자료구조 / 자바 / LIST,ARRAY

---
+++ data_structure/array/src/array/array_1.java
@@ -0,0 +1,17 @@
+package array;
+
+import java.util.Arrays;
+
+public class array_1 {
+	public static void main(String[] args) {
+		int [] arr= new int[10];
+		for(int i=0; i<10;i++) {
+			arr[i]=i;
+		}
+		
+		System.out.println(Arrays.toString(arr));
+		
+	}
+	
+
+}
---
+++ data_structure/array/src/list_1/List_1.java
@@ -0,0 +1,11 @@
+package list_1;
+
+import java.util.List;
+
+public class List_1 {
+
+	public static void main(String[] args) {
+		List<String >list = {}; 
+	}
+
+}
