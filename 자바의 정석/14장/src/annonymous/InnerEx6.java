d44d9d2125e5cdb48172aad235584f36813732d9
kimth007kim <kimth007kim@naver.com>
2022-12-28 21:22:07 +0900

    자바의정석/ 14장 람다 완료

---
+++ 자바의 정석/14장/src/annonymous/InnerEx6.java
@@ -0,0 +1,22 @@
+package annonymous;
+
+public class InnerEx6 {
+    Object iv = new Object() {
+        void method() {
+
+        }
+    };
+
+    static Object cv = new Object() {
+        void method() {
+        }
+    };
+
+    void myMethod() {
+        Object lv = new Object() {
+            void method() {
+            }
+        };
+    }
+
+}
---
+++ 자바의 정석/14장/src/annonymous/InnerEx7.java
@@ -0,0 +1,21 @@
+package annonymous;
+
+//import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class InnerEx7 {
+    public static void main(String[] args) {
+        Button b= new Button("start");
+        b.addActionListener(new EventHandler());
+    }
+
+}
+
+class EventHandler implements ActionListener{
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("ActionEvent occurred!!!!");
+    }
+}
