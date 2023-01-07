dde73cbc8ba5063a90d1349aefcc011ddffe790a
kimth007kim <kimth007kim@naver.com>
2023-01-07 22:24:42 +0900

    자바의정석/ 15장 입출력 I/O 완료

---
+++ 자바의 정석/15장/src/fileEx1/FileEx1.java
@@ -0,0 +1,31 @@
+package fileEx1;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileEx1 {
+    public static void main(String[] args) throws IOException {
+        File f = new File("C:\\Users\\Kyungdong\\Documents\\cs_study\\자바의 정석\\15장\\abc.java");
+        String fileName = f.getName();
+        int pos=fileName.lastIndexOf(".");
+        
+        
+        System.out.println("경로를 제외한 파일이름 - "+f.getName());
+        System.out.println("확장자를 제외한 파일이름 - "+fileName.substring(0,pos));
+        System.out.println("확장자 - "+fileName.substring(pos+1));
+
+        System.out.println("경로를 포함한 파일이름 - "+f.getPath());
+        System.out.println("파일의 절대경로 - "+f.getAbsolutePath());
+        System.out.println("파일의 정규경로 - "+f.getCanonicalPath());
+        System.out.println("파일이 속해 있는 디렉토리 - "+f.getParent());
+        System.out.println();
+
+        System.out.println("File.separator - "+File.pathSeparator);
+        System.out.println("File.pathSeparatorChar - "+File.pathSeparatorChar);
+        System.out.println();
+        System.out.println("user.dir="+System.getProperty("user.dir"));
+
+
+
+    }
+}
---
+++ 자바의 정석/15장/src/fileReaderEx1/FileReaderEx1.java
@@ -0,0 +1,35 @@
+package fileReaderEx1;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderEx1 {
+    public static void main(String[] args) {
+        try {
+            String fileName = "test.txt";
+            FileInputStream fis = new FileInputStream(fileName);
+            FileReader fr = new FileReader(fileName);
+
+            int data = 0;
+
+            while ((data = fis.read()) != -1) {
+                System.out.print((char) data);
+            }
+            System.out.println();
+            fis.close();
+            while ((data = fr.read()) != -1) {
+                System.out.print((char) data);
+            }
+            fr.close();
+
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
